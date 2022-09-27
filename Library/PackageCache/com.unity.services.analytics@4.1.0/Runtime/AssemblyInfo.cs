62181aa7d9f5671efb347dd0853a1b272298dd70
piaomz <piaomz1999@gmail.com>
2022-09-27 02:27:19 -0700

    Level1(Scene1) SceneSwitcher LibraryRemove&Ignore(LargeFileError)

--- Library/PackageCache/com.unity.services.analytics@4.1.0/Runtime/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Unity.Services.Analytics.DataPrivacy")]
-[assembly: InternalsVisibleTo("Unity.Services.Analytics.Tests")]
-[assembly: InternalsVisibleTo("Unity.Services.Analytics.EditorTests")]
-[assembly: InternalsVisibleTo("Unity.Services.AnalyticsRuntime.Tests")]
--- README.txt
+++
@@ -1,2 +0,0 @@
-Play ME!!!
-https://play.unity.com/mg/other/webgl-builds-244035
