3d8e8589f1bdc53b548088d78dcfa237300f5206
Frank Yao <1404815414@qq.com>
2022-10-15 23:51:28 -0700

    smoothly zoom camera

--- Assets/Scripts/ManualBoardControl.cs
+++ Assets/Scripts/ManualBoardControl.cs
@@ -30,7 +30,7 @@ public class ManualBoardControl : MonoBehaviour
             }
             else
             {
-                ManualBoard.text = "c          :show and hide manual\nwasd  :control player 1\n↑↓←→:control player 2\nv         :separate view or not\nb         :change view angle";
+                ManualBoard.text = "c          :show and hide manual\nwasd  :control player 1\n↑↓←→:control player 2\nv         :separate view or not\nb         :change view angle\nspace :press to see widerly temporally";
                 show = true;
             }
         }
