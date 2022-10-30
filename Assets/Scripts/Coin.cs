818e69788a47ae5e979da85a79f4c85fcd6678a9
piaomz <piaomz1999@gmail.com>
2022-10-30 01:49:43 -0700

    pauseButtonAdded2

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -12,7 +12,7 @@ public class Coin : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         transform.Rotate(new Vector3(1,0,0));
     }
