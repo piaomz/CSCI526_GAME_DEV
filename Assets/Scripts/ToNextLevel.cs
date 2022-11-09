983b93caccb6cb56ab1187e61f5cb8a4ea15d7aa
reginaHuPeifeng <peifengh@usc.edu>
2022-11-08 20:46:46 -0800

    add level lock

--- Assets/Scripts/ToNextLevel.cs
+++ Assets/Scripts/ToNextLevel.cs
@@ -8,9 +8,11 @@ public class ToNextLevel : MonoBehaviour
     public GameObject Button;
     public GameObject congratulationText;
     public string nextLevelName;
+    public int nextSceneLoad;
     // Start is called before the first frame update
     void Start()
     {
+        nextSceneLoad = SceneManager.GetActiveScene().buildIndex + 1;
         Button.GetComponent<Button>().onClick.AddListener(NextLevel);
     }
     
@@ -18,5 +20,9 @@ public class ToNextLevel : MonoBehaviour
         Debug.Log("nextlevel");
         Time.timeScale = 1;
         SceneManager.LoadScene(nextLevelName);
+        if (nextSceneLoad > PlayerPrefs.GetInt("levelAt"))
+        {
+            PlayerPrefs.SetInt("levelAt", nextSceneLoad);
+        }
     }
 }
