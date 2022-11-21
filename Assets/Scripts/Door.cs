23b25ceeea37253c2a087f26803ef7b5e4d01e5c
piaomz <piaomz1999@gmail.com>
2022-11-21 00:15:31 -0800

    doorAnime

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -8,25 +8,29 @@ public class Door : MonoBehaviour
     public int flagA = 0;
     public int flagB = 0;
     public int isOpen=0;
+    private int isMove2Place=0;
+    private float initalY;
     // Start is called before the first frame update
     void Start()
     {
-        
+        initalY=transform.position.y;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(isOpen==1 && isMove2Place==0){
+            transform.position = new Vector3(transform.position.x,transform.position.y + 0.05f, transform.position.z);
+            if(transform.position.y>=initalY+1.5f){
+                isMove2Place=1;
+            }
+        }
     }
     public void Lift()
     {
         if (flagA ==1 && flagB==1 && isOpen==0) {
             isOpen=1;
-            transform.position = new Vector3(transform.position.x,
-                transform.position.y + (float)1.5, transform.position.z);
             sending.UpdateCheckPoint(gameObject.name);
         }
-        
     }
 }
