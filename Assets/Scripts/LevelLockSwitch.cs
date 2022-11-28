ed69b1675a473292360e5c486b97823cfbbc6151
Regina Hu <peifengh@usc.edu>
2022-11-28 02:54:36 -0800

    functinal level lock/unlock button

--- Assets/Scripts/LevelLockSwitch.cs
+++ Assets/Scripts/LevelLockSwitch.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events; // This is so that you can extend the pointer handlers
+using UnityEngine.EventSystems; // This is so that you can extend the pointer handlers
 
 public class LevelLockSwitch : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Button[] lvlButtons;
+
+    int levelAt;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LevelUnlock()
     {
-        
+        if (lvlButtons[lvlButtons.Length - 1].interactable == true)
+        {
+            levelAt = PlayerPrefs.GetInt("levelAt", 1);
+            for (int i = 0; i < lvlButtons.Length; i++)
+            {
+                if (i + 1 > levelAt)
+                    lvlButtons[i].interactable = false;
+            }
+            PlayerPrefs.SetInt("levelAt", 1);
+        }
+        else
+        {
+            for (int i = 0; i < lvlButtons.Length; i++)
+            {
+                    lvlButtons[i].interactable = true;
+            }
+            PlayerPrefs.SetInt("levelAt", SceneManager.sceneCountInBuildSettings-1);
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
